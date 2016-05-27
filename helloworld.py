639d11d965a3cfc4af32cab54197756d88b1e6d5
Tobias Hornberger <saibot1207@googlemail.com>
2016-05-27 22:58:55 +0200

    setup and all that

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy, time, sys
+from keys import *
+
+argfile = str(sys.argv[1])
+
+#enter the corresponding information from your Twitter application:
+
+#Your Keys are in another castle
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+print(ACCESS_KEY)
+filename=open(argfile,'r')
+f=filename.readlines()
+filename.close()
+
+for line in f:
+        api.update_status(line)
+        time.sleep(900)#Tweet every 15 minutes
